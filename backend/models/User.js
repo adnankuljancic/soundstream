78ff692aa6a20985706dd7639db051c9d6538ac9
Tarik <tarik905.suljic@hotmail.com>
2023-05-30 14:14:24 +0200

    Added createdAt,updatedAt fields to the User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,24 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Schema=mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-      type: String,
-      required: true
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    password: {
-      type: String,
-      required: true
-    },
-  });
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: String,
+  updatedAt: String,
+});
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports=User;
-  
+module.exports = User;
--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,15 +1,24 @@
-const User = require('../models/User.js');
+const User = require("../models/User.js");
+const { DateTime } = require("luxon");
 
-async function createUser(username,email,password){
-    try{
-        const newUser=await User.create({username,email,password});
-        return newUser;
-    }catch(error){
-        console.log(error);
-        throw new Error('Failed to create User');
-    }
+async function createUser(username, email, password) {
+  var createdAt = DateTime.now().toISO();
+  var updatedAt = DateTime.now().toISO();
+  try {
+    const newUser = await User.create({
+      username,
+      email,
+      password,
+      createdAt,
+      updatedAt,
+    });
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to create User");
+  }
 }
 
 module.exports = {
-    createUser,
-  };
+  createUser,
+};
