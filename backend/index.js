12ded819d769c5151b292cddbd787488bd4294b4
Tarik <tarik905.suljic@hotmail.com>
2023-06-14 12:30:27 +0200

    Added song routes

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,28 @@
-require('dotenv').config();
-const express=require('express');
-const mongoose=require('mongoose');
-const userRoutes=require('./routes/userRoutes');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const songRoutes = require("./routes/songRoutes");
 
-const app=express();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/users',userRoutes);
+app.use("/users", userRoutes);
+app.use("/songs", songRoutes);
 
 //Database connection
 const connectionString = process.env.DB;
-mongoose.connect(connectionString,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-})
-.then(()=>{
-    console.log('Connected to MongoDB');
-})
-.catch((error)=>{
-    console.error('Error connecting to MongoDB: ',error);
-})
-module.exports=app;
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB: ", error);
+  });
+module.exports = app;
