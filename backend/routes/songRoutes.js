5211087e2f5fbfb8a51e464e78b45863d64c5bbb
Tarik <tarik905.suljic@hotmail.com>
2023-06-15 14:13:29 +0200

    added delete route

--- backend/routes/songRoutes.js
+++ backend/routes/songRoutes.js
@@ -6,5 +6,6 @@ const songController = require("../controllers/songController");
 const router = express.Router();
 
 router.post("/upload", upload.single("file"), songController.uploadSong);
+router.delete("/remove", songController.removeSong);
 
 module.exports = router;
