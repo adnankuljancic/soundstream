a61d89dc79b6c96c609ee01937d6aac47ebe972f
Tarik <tarik905.suljic@hotmail.com>
2023-06-18 14:46:03 +0200

    update navbar after login

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,25 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {LinkContainer} from 'react-router-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap';
+import {useState,useEffect,useContext} from 'react';
+import {useNavigate} from 'react-router-dom';
+import UserContext from '../../context/UserContext';
 
 export const MainNavBar = () => {
+  const [isLoggedIn, setIsLoggedIn]=useState(false);
+  const {userData, setUserData}=useContext(UserContext);
+  const navigate=useNavigate();
+
+  useEffect(() => {
+    
+  }, [userData]);
+
+  const handleLogout = () => {
+    setUserData(null);
+    navigate("/");
+  };
+
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
@@ -14,15 +30,28 @@ export const MainNavBar = () => {
             <LinkContainer to="/">
               <Nav.Link>Home</Nav.Link>
             </LinkContainer>
-            <LinkContainer to="/liked-songs">
-              <Nav.Link>Liked Songs</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <Nav.Link>Log In</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/register">
-              <Nav.Link>Register</Nav.Link>
-            </LinkContainer>
+            
+            {userData!=null && (
+              <LinkContainer to="/liked-songs">
+                <Nav.Link>My Songs</Nav.Link>
+              </LinkContainer>
+            )}
+            {userData!=null ? (
+              <>
+                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+                <Nav.Link>{userData.fullName}</Nav.Link>
+
+              </>
+            ) : (
+              <>
+                <LinkContainer to="/login">
+                  <Nav.Link>Log In</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/register">
+                  <Nav.Link>Register</Nav.Link>
+                </LinkContainer>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
