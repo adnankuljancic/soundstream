cc18daf49d10ee427a9754fe2b4308afd6c03dea
Tarik <tarik905.suljic@hotmail.com>
2023-06-18 14:46:44 +0200

    implementing UserContext.Provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,46 @@
-
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './components/Home/Home';
-import { LogIn } from './components/LogIn/LogIn';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { MainNavBar } from './components/NavBar/NavBar';
-import { Register } from './components/Register/Register';
-import { PageNotFound } from './components/PageNotFound/PageNotFound';
-import { LikedSongs } from './components/LikedSongs/LikedSongs';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home/Home";
+import { LogIn } from "./components/LogIn/LogIn";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { MainNavBar } from "./components/NavBar/NavBar";
+import { Register } from "./components/Register/Register";
+import { PageNotFound } from "./components/PageNotFound/PageNotFound";
+import { LikedSongs } from "./components/LikedSongs/LikedSongs";
+import UserContext from "./context/UserContext";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const loggedIn = async () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        setUserData({
+          fullName: "Tarik Suljic",
+          token: token,
+        });
+        return true;
+      } else return false;
+    };
+
+    loggedIn();
+  }, []);
+
   return (
     <div className="App">
       <Router>
-        <div>
-          <MainNavBar/>
-        </div>
-        <Routes>
-          <Route path='/' element={ <Home/> }/>
-          <Route path='/login' element={ <LogIn/> }/>
-          <Route path='/register' element={ <Register/> }/>
-          <Route path='/liked-songs' element={ <LikedSongs/> }/>
-          <Route path='/*' element={ <PageNotFound/> }/>
-        </Routes>
+        <UserContext.Provider value={{ userData, setUserData }}>
+          <MainNavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/liked-songs" element={<LikedSongs />} />
+            <Route path="/*" element={<PageNotFound />} />
+          </Routes>
+        </UserContext.Provider>
       </Router>
     </div>
   );
