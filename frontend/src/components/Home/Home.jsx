d226a3a15dc3a31534c84ac59064b4b7f8eaf1be
Tarik <tarik905.suljic@hotmail.com>
2023-06-21 16:36:53 +0200

    fetched songs on homepage

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,14 +1,41 @@
 import { CardGroup } from 'react-bootstrap';
 import { PlaylistCard } from '../PlaylistCard/PlaylistCard';
+import axios from 'axios';
+import {useEffect,useState} from 'react'; 
+import AudioCard from '../AudioCard/AudioCard';
 export const Home = () => {
+  const [songs, setSongs] = useState([]);
+
+
+  useEffect(() => {
+
+    // Fetch songs from the database
+    axios.get('http://localhost:3001/songs')
+      .then(response => {
+        const songsData = response.data;
+        setSongs(songsData);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className='container my-3'>
       <h1 className='mt-5'>Welcome to SoundStream!</h1>
       <p className='mb-5'>Select a playlist and enjoy listening.</p>
+      
       <CardGroup>
-      <PlaylistCard></PlaylistCard>
-      <PlaylistCard></PlaylistCard>
-      <PlaylistCard></PlaylistCard>
+        <div className="container">
+           <div className="row g-4">
+    
+              {songs.map(song => (<div key={song.id} className="col-3">
+                  <AudioCard song={song} />
+                       </div>))}
+    
+        </div>
+</div>
+        
       </CardGroup>
     </div>
 
